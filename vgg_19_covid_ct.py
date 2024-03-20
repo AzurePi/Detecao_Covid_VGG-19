ee11ff8599b531e37a36a03e27e84698fb2aed57
azure <ppcardana@hotmail.com>
2024-03-20 20:54:01 -0300

    made the code work with the environment

--- vgg_19_covid_ct.py
+++ vgg_19_covid_ct.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Importações
+# Importações ----------------------------------------------------------------------------------------------------------
 
 import os
 import shutil
@@ -8,91 +8,93 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 import random
 
-os.chdir("/home/azure/")
-
-# @title Integração com o Kaggle
-kaggle_dir = os.path.expanduser('~/.kaggle')
-if not os.path.exists(kaggle_dir):
-    os.mkdir(kaggle_dir)
-
 """# Preparação do Dataset"""
 
-# @title Download a partir do Kaggle
+# @title Download a partir do Kaggle e formatação do diretóro do dataset  ----------------------------------------------
 '''
   Se o dataset ainda não foi baixado, baixamos e descompactamos
   Mantemos apenas as imagens originais, e não as pré-processadas (faremos o pré-processamento nós mesmos)
 '''
-if not os.path.exists("/VGG_19_Covid/dataset"):
-    os.system('kaggle datasets download -d azaemon/preprocessed-ct-scans-for-covid19 --unzip -q')
-    print("Dataset download sucessful")
-    os.rename("/VGG_19_Covid/preprocessed-ct-scans-for-covid19/", "/VGG_19_Covid/dataset")
-    print("Dataset folder renamed sucessfuly")
+if not os.path.exists("./dataset"):
+    os.mkdir("./dataset")
 
-    os.system(
-        'mv "/VGG_19_Covid/dataset/Original CT Scans/NiCT" "/VGG_19_Covid/dataset/Original CT Scans/nCT" "/VGG_19_Covid/dataset"')
-    os.system('rm -r "/VGG_19_Covid/dataset/Original CT Scans"')
+    os.system('kaggle datasets download -d azaemon/preprocessed-ct-scans-for-covid19 -p ./dataset --unzip -q')
+    shutil.rmtree("./dataset/Preprocessed CT scans")
 
-    dir = os.listdir("/VGG_19_Covid/dataset/NiCT")
-    for i in dir:
-        shutil.move(os.path.join("/VGG_19_Covid/dataset/NiCT", i), "/VGG_19_Covid/dataset/nCT/")
+    directories = [d for d in os.listdir("./dataset/Original CT Scans") if
+                   os.path.isdir(os.path.join("./dataset/Original CT Scans", d))]
 
-    os.rename("/VGG_19_Covid/dataset/nCT", "/VGG_19_Covid/dataset/negative")
-    os.rename("/VGG_19_Covid/dataset/pCT", "/VGG_19_Covid/dataset/positive")
+    for directory in directories:
+        src = os.path.join("./dataset/Original CT Scans", directory)
+        dest = os.path.dirname("./dataset/Original CT Scans")
+        print(dest)
+        shutil.move(src, dest)
+    os.rmdir("./dataset/Original CT Scans")
 
-    os.makedirs("/VGG_19_Covid/dataset/training/positive/", exist_ok=True)
-    os.makedirs("/VGG_19_Covid/dataset/training/negative/", exist_ok=True)
-    os.makedirs("/VGG_19_Covid/dataset/validation/positive/", exist_ok=True)
-    os.makedirs("/VGG_19_Covid/dataset/validation/negative/", exist_ok=True)
+    for file_name in os.listdir("./dataset/NiCT"):
+        src = os.path.join("./dataset/NiCT", file_name)
+        dest = os.path.join("./dataset/nCT", file_name)
+        shutil.move(src, dest)
+    os.rmdir("./dataset/NiCT")
 
-# @title Restrição do tamanho do dataset
+    os.rename("./dataset/nCT", "./dataset/negative")
+    os.rename("./dataset/pCT", "./dataset/positive")
+
+# @title Restrição do tamanho do dataset -------------------------------------------------------------------------------
 # utilizando um sampling randomizado, separamos as imagens em conjuntos de treinamento e validação
 
 sampling_training = 0.4
 sampling_validation = 0.2
 
-path = "/VGG_19_Covid/dataset/"
+if not (os.path.exists("./training")):
+    os.mkdir("./training")
+    os.mkdir("./training/positive")
+    os.mkdir("./training/negative")
 
-# selecionamos 30% das imagens positivas para treinamento
-dir = os.listdir(path + "positive")
-dir = random.sample(dir, (int)(len(dir) * sampling_training))
-source = path + "positive/"
-for imagem in dir:
-    shutil.move(source + imagem, path + "training/positive/")
+    # selecionamos 40% das imagens positivas para treinamento
+    dir = os.listdir("./dataset/positive")
+    dir = random.sample(dir, (int)(len(dir) * sampling_training))
+    source = "./dataset/positive/"
+    for imagem in dir:
+        shutil.copy(source + imagem, "./training/positive/")
 
-# selecionamos 30% das imagens negativas para treinamento
-dir = os.listdir(path + "negative")
-dir = random.sample(dir, (int)(len(dir) * sampling_training))
-source = path + "negative/"
-for imagem in dir:
-    shutil.move(source + imagem, path + "training/negative/")
+    # selecionamos 40% das imagens negativas para treinamento
+    dir = os.listdir("./dataset/negative")
+    dir = random.sample(dir, (int)(len(dir) * sampling_training))
+    source = "./dataset/negative/"
+    for imagem in dir:
+        shutil.copy(source + imagem, "./training/negative/")
 
-# selecionamos 10% das imagens positivas para validação
-dir = os.listdir(path + "positive")
-dir = random.sample(dir, (int)(len(dir) * sampling_validation))
-source = path + "positive/"
-for imagem in dir:
-    shutil.move(source + imagem, path + "validation/positive/")
+if not (os.path.exists("./validation")):
+    os.mkdir("./validation")
+    os.mkdir("./validation/positive")
+    os.mkdir("./validation/negative")
 
-# selecionamos 10% das imagens negativas para validação
-dir = os.listdir(path + "negative")
-dir = random.sample(dir, (int)(len(dir) * sampling_validation))
-source = path + "negative/"
-for imagem in dir:
-    shutil.move(source + imagem, path + "validation/negative/")
+    # selecionamos 20% das imagens positivas para validação
+    dir = os.listdir("./dataset/positive")
+    dir = random.sample(dir, (int)(len(dir) * sampling_validation))
+    source = "./dataset/positive/"
+    for imagem in dir:
+        shutil.copy(source + imagem, "./validation/positive/")
 
-os.system('rm -r "/VGG_19_Covid/dataset/negative"')
-os.system('rm -r "/VGG_19_Covid/dataset/positive"')
+    # selecionamos 20% das imagens negativas para validação
+    dir = os.listdir("./dataset/negative")
+    dir = random.sample(dir, (int)(len(dir) * sampling_validation))
+    source = "./dataset/negative/"
+    for imagem in dir:
+        shutil.copy(source + imagem, "./validation/negative/")
 
-# @title Criação de dataset keras
+
+# @title Criação de dataset keras --------------------------------------------------------------------------------------
 train_ds = tf.keras.utils.image_dataset_from_directory(
-    directory="/VGG_19_Covid/dataset/training",
+    directory="./training",
     labels="inferred",
     label_mode="categorical",
     batch_size=32,
     image_size=(256, 256))
 
 validation_ds = tf.keras.utils.image_dataset_from_directory(
-    directory="/VGG_19_Covid/dataset/validation",
+    directory="./validation",
     labels="inferred",
     label_mode="categorical",
     batch_size=32,
@@ -109,7 +111,7 @@ for i, layer in enumerate(VGG_19_base.layers):
 
 print(VGG_19_base.summary())
 
-# @title Layers para fine-tuning
+# @title Layers para fine-tuning ---------------------------------------------------------------------------------------
 FC_layer_Flatten = tf.keras.layers.Flatten()(VGG_19_base.output)
 Dense = tf.keras.layers.Dense(units=200, activation="relu")(FC_layer_Flatten)
 Dense = tf.keras.layers.Dense(units=160, activation="relu")(Dense)
@@ -146,3 +148,6 @@ plt.legend(['train', 'validation'])
 plt.show()
 
 prediction = modelo_final.predict(validation_ds)
+
+shutil.rmtree("./training")
+shutil.rmtree("./validation")
