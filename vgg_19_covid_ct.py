eec9d48d0a7eb51ac5725ad8fe824918dc986351
azure <ppcardana@hotmail.com>
2024-04-04 20:27:10 -0300

    major fixes on hte dataset extraction

--- vgg_19_covid_ct.py
+++ vgg_19_covid_ct.py
@@ -14,12 +14,10 @@ from matplotlib.ticker import MaxNLocator
 import random
 import datetime
 
-"""# Preparação do Dataset"""
-
-# @title Download a partir do Kaggle e formatação do diretóro do dataset  ----------------------------------------------
+# Download a partir do Kaggle, pré-processamento e formatação do diretório ---------------------------------------------
 '''
   Se o dataset ainda não foi baixado, baixamos e descompactamos
-  Mantemos apenas as imagens originais, e não as pré-processadas (faremos o pré-processamento nós mesmos)
+  Mantemos apenas as imagens originais, e não as pré-processadas
 '''
 if not os.path.exists("./dataset"):
     os.mkdir("./dataset")
@@ -46,62 +44,82 @@ if not os.path.exists("./dataset"):
     os.rename("./dataset/nCT", "./dataset/negative")
     os.rename("./dataset/pCT", "./dataset/positive")
 
-# @title Restrição do tamanho do dataset -------------------------------------------------------------------------------
-# utilizando um sampling randomizado, separamos as imagens em conjuntos de treinamento e validação
+# Restrição do tamanho do dataset --------------------------------------------------------------------------------------
+num_positivas = len(os.listdir("./dataset/positive"))
+num_negativas = len(os.listdir("./dataset/negative"))
 
-sampling_training = 0.3
-sampling_validation = 0.3
+# dividimos as imagens em terços
+num_positivas_por_conjunto = num_positivas // 3
+num_negativas_por_conjunto = num_negativas // 3
 
-os.mkdir("./temp")
-shutil.copytree("./dataset", "./temp", dirs_exist_ok=True)
+if not (os.path.exists("./training") & os.path.exists("./validation") & os.path.exists("./teste")):
+    os.mkdir("./temp")
+    shutil.copytree("./dataset", "./temp", dirs_exist_ok=True)
 
-if not (os.path.exists("./training")):
-    os.mkdir("./training")
-    os.mkdir("./training/positive")
-    os.mkdir("./training/negative")
+    if not (os.path.exists("./training")):
+        os.mkdir("./training")
+        os.mkdir("./training/positive")
+        os.mkdir("./training/negative")
 
-    # selecionamos uma fração das imagens positivas para treinamento
-    diretorio = os.listdir("./temp/positive")
-    diretorio = random.sample(diretorio, int(len(diretorio) * sampling_training))
-    source = "./temp/positive/"
-    for imagem in diretorio:
-        shutil.move(f'{source}{imagem}', "./training/positive/")
+        diretorio = os.listdir("./temp/positive")
+        random.shuffle(diretorio)
+        source = "./temp/positive/"
+        for imagem in diretorio[:num_positivas_por_conjunto]:
+            shutil.move(f'{source}{imagem}', "./training/positive/")
 
-    # selecionamos uma fração das imagens negativas para treinamento
-    diretorio = os.listdir("./temp/negative")
-    diretorio = random.sample(diretorio, int(len(diretorio) * sampling_training))
-    source = "./temp/negative/"
-    for imagem in diretorio:
-        shutil.move(f'{source}{imagem}', "./training/negative/")
+        diretorio = os.listdir("./temp/negative")
+        random.shuffle(diretorio)
+        source = "./temp/negative/"
+        for imagem in diretorio[:num_negativas_por_conjunto]:
+            shutil.move(f'{source}{imagem}', "./training/negative/")
 
-if not (os.path.exists("./validation")):
-    os.mkdir("./validation")
-    os.mkdir("./validation/positive")
-    os.mkdir("./validation/negative")
+    if not (os.path.exists("./validation")):
+        os.mkdir("./validation")
+        os.mkdir("./validation/positive")
+        os.mkdir("./validation/negative")
 
-    # selecionamos uma fração das imagens positivas para validação
-    diretorio = os.listdir("./temp/positive")
-    diretorio = random.sample(diretorio, int(len(diretorio) * sampling_validation))
-    source = "./temp/positive/"
-    for imagem in diretorio:
-        shutil.move(f'{source}{imagem}', "./validation/positive/")
+        # selecionamos uma fração das imagens positivas para validação
+        diretorio = os.listdir("./temp/positive")
+        random.shuffle(diretorio)
+        source = "./temp/positive/"
+        for imagem in diretorio[:num_positivas_por_conjunto]:
+            shutil.move(f'{source}{imagem}', "./validation/positive/")
 
-    # selecionamos uma fração das imagens negativas para validação
-    diretorio = os.listdir("./temp/negative")
-    diretorio = random.sample(diretorio, int(len(diretorio) * sampling_validation))
-    source = "./temp/negative/"
-    for imagem in diretorio:
-        shutil.move(f'{source}{imagem}', "./validation/negative/")
+        # selecionamos uma fração das imagens negativas para validação
+        diretorio = os.listdir("./temp/negative")
+        random.shuffle(diretorio)
+        source = "./temp/negative/"
+        for imagem in diretorio[:num_negativas_por_conjunto]:
+            shutil.move(f'{source}{imagem}', "./validation/negative/")
 
-shutil.rmtree("./temp")
+    if not (os.path.exists("./teste")):
+        os.mkdir("./teste")
+        os.mkdir("./teste/positive")
+        os.mkdir("./teste/negative")
 
-# @title Criação de dataset keras --------------------------------------------------------------------------------------
+        diretorio = os.listdir("./temp/positive")
+        random.shuffle(diretorio)
+        source = "./temp/positive/"
+        for imagem in diretorio[:num_positivas_por_conjunto]:
+            shutil.move(f'{source}{imagem}', "./teste/positive/")
+
+        # selecionamos uma fração das imagens negativas para validação
+        diretorio = os.listdir("./temp/negative")
+        random.shuffle(diretorio)
+        source = "./temp/negative/"
+        for imagem in diretorio[:num_negativas_por_conjunto]:
+            shutil.move(f'{source}{imagem}', "./teste/negative/")
+
+    shutil.rmtree("./temp")
+
+# Criação de datasets keras --------------------------------------------------------------------------------------------
 train_ds = tf.keras.utils.image_dataset_from_directory(
     directory="./training",
     labels="inferred",
     label_mode="categorical",
     batch_size=32,
     image_size=(256, 256))
+train_ds = train_ds.map(lambda x, y: (tf.keras.applications.vgg19.preprocess_input(x), y))
 
 validation_ds = tf.keras.utils.image_dataset_from_directory(
     directory="./validation",
@@ -109,9 +127,17 @@ validation_ds = tf.keras.utils.image_dataset_from_directory(
     label_mode="categorical",
     batch_size=32,
     image_size=(256, 256))
+validation_ds = validation_ds.map(lambda x, y: (tf.keras.applications.vgg19.preprocess_input(x), y))
 
-"""# Aplicação da VGG-19"""
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    directory="./validation",
+    labels="inferred",
+    label_mode="categorical",
+    batch_size=32,
+    image_size=(256, 256))
+test_ds = test_ds.map(lambda x, y: (tf.keras.applications.vgg19.preprocess_input(x), y))
 
+# Aplicação da VGG-19 --------------------------------------------------------------------------------------------------
 image_input = tf.keras.layers.Input(shape=(256, 256, 3))
 VGG_19_base = tf.keras.applications.VGG19(include_top=False, weights="imagenet", input_tensor=image_input,
                                           pooling="max", classifier_activation="softmax")
@@ -121,30 +147,38 @@ for i, layer in enumerate(VGG_19_base.layers):
 
 print(VGG_19_base.summary())
 
-# @title Layers para fine-tuning ---------------------------------------------------------------------------------------
+# Layers para fine-tuning
 FC_layer_Flatten = tf.keras.layers.Flatten()(VGG_19_base.output)
 Dense = tf.keras.layers.Dense(units=256, activation="relu")(FC_layer_Flatten)
 Dense = tf.keras.layers.Dense(units=128, activation="relu")(Dense)
 Dense = tf.keras.layers.Dense(units=64, activation="relu")(Dense)
 Dense = tf.keras.layers.Dense(units=32, activation="relu")(Dense)
 Dense = tf.keras.layers.Dense(units=16, activation="relu")(Dense)
+Dense = tf.keras.layers.Dense(units=8, activation="relu")(Dense)
+Dense = tf.keras.layers.Dense(units=4, activation="relu")(Dense)
 Classification = tf.keras.layers.Dense(units=2, activation="softmax")(Dense)
 
+# Compilação do modelo final
 modelo_final = tf.keras.Model(inputs=image_input, outputs=Classification)
 modelo_final.summary()
 
 base_learning_rate = 0.001
-epocas = 100
+epocas =3
 
 modelo_final.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
                      loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
 
 csv_logger = CSVLogger("training.csv", ",", False)  # cria um log em CSV para armazenar a história do modelo
 
+# Treinamento
 start_time = datetime.datetime.now()
 history = modelo_final.fit(x=train_ds, epochs=epocas, validation_data=validation_ds, callbacks=[csv_logger], verbose=2)
 end_time = datetime.datetime.now()
 
+# Teste
+test_score = modelo_final.evaluate(test_ds)
+
+# Registro dos resultados ----------------------------------------------------------------------------------------------
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
 plt.title("Acurácia do Modelo")
@@ -165,9 +199,9 @@ plt.legend(['train', 'validation'])
 plt.savefig("perda.png", bbox_inches='tight')
 plt.close()
 
-with open("parametros.txt", "w") as file:
-    file.write(f"Sampling do training set: {sampling_training}")
-    file.write(f"Sampling do training set: {sampling_validation}")
-    file.write(f"Learning rate: {base_learning_rate}")
-    file.write(f"Épocas: {epocas}")
-    file.write(f"Tempo decorrido: {end_time-start_time}")
+with open("info.txt", "w") as file:
+    file.write(f"Learning rate: {base_learning_rate}\n")
+    file.write(f"Épocas: {epocas}\n")
+    file.write(f"Tempo decorrido: {end_time - start_time}\n")
+    file.write(f"Acurácia do deste: {test_score[1]}")
+    file.write(f"Perda do deste: {test_score[0]}")
