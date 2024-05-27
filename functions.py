f9d11ac6ffbe4e904f6b72822f485e5666cb9447
azure <ppcardana@hotmail.com>
2024-05-26 23:33:42 -0300

    melhora no projeto

--- functions.py
+++ functions.py
@@ -1,14 +1,66 @@
 import os
 import shutil
+from datetime import timedelta
 from pathlib import Path
 
+import keras.callbacks
 import numpy as np
+import tensorflow as tf
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 
-def prepararDiretorios(dataset_dir1, dataset_dir2, test_split):
-    if not Path("resultados").exists():
-        Path("resultados").mkdir()
+def salvar_parametros(base_learning_rate: float, epocas: int, test_split: float, validation_split: float) -> None:
+    """
+    Salva os parâmetros do treinamento em um arquivo "resultados/parametros.txt".
+
+    :param base_learning_rate: Taxa de aprendizado utilizada no treinamento.
+    :param epocas: Número de épocas do treinamento.
+    :param test_split: Proporção dos dados utilizados para teste.
+    :param validation_split: Proporção dos dados utilizados para validação.
+    :return: None
+    """
+    with open("./resultados/parametros.txt", 'w') as file:
+        file.write(f"Learning rate: {base_learning_rate}\n")
+        file.write(f"Épocas: {epocas}\n")
+        file.write(f"Test split: {test_split}\n")
+        file.write(f"Validation split: {validation_split}\n")
+        file.close()
+
+
+def prepararResultados() -> tuple[Path, int]:
+    """
+    Prepara a estrutura de diretórios para armazenar os resultados do treinamento. Calcula o número de treinamentos
+    já feitos com base na quantidade de arquivos em "./resultados/training/normal".
+
+    Verifica se os diretórios "./resultados", "./resultados/training", "./resultados/training/normal" e
+    "./resultados/training/transfer" existem. Se não existirem, cria esses diretórios.
+
+    :return: Uma tupla contendo o caminho do diretório de treinamento e o número de treinamentos já feitos.
+    """
+    if not Path("./resultados").exists():
+        Path("./resultados").mkdir()
+    if not Path("./resultados/training").exists():
+        Path("./resultados/training").mkdir()
+    if not Path("./resultados/training/normal").exists():
+        Path("./resultados/training/normal").mkdir()
+    if not Path("./resultados/training/transfer").exists():
+        Path("./resultados/training/transfer").mkdir()
+
+    training = Path("./resultados/training")
+    n = np.ceil(len(list((training / "normal").glob("*"))) / 2)
+    return training, n
+
+
+def prepararDiretorios(dataset_dir1: Path, dataset_dir2: Path, test_split: float) -> tuple[list[int], list[int]]:
+    """
+    Prepara os diretórios e conjuntos de dados para treinamento e teste.
+
+    :param dataset_dir1: Diretório do primeiro conjunto de dados.
+    :param dataset_dir2: Diretório do segundo conjunto de dados.
+    :param test_split: Proporção dos dados a serem utilizados para teste.
+    :return: Uma lista contendo duas sublistas, cada uma com o número de exemplos positivos e negativos para os dois conjuntos de dados.
+    """
 
     preparar_dataset1(dataset_dir1)
     num_positivas1 = len(list((dataset_dir1 / "positivo").glob("*")))
@@ -20,15 +72,16 @@ def prepararDiretorios(dataset_dir1, dataset_dir2, test_split):
     num_negativas2 = len(list((dataset_dir2 / "negativo").glob("*")))
     treinamento_e_teste(2, num_positivas2, num_negativas2, test_split, dataset_dir2)
 
-    return num_positivas1 + num_negativas1, num_positivas2 + num_negativas2
+    return [num_positivas1, num_negativas1], [num_positivas2, num_negativas2]
 
 
-def formatar_diretorio(origem, destino):
-    """Move todos os arquivos de um diretório de origem para um destino e remove o diretório de origem.
+def formatar_diretorio(origem: Path, destino: Path) -> None:
+    """
+    Move todos os arquivos de um diretório de origem para um destino e remove o diretório de origem.
 
     :param origem: Diretório de origem contendo os arquivos.
     :param destino: Diretório de destino.
-    :return:
+    :return: None
     """
     if not destino.exists():
         destino.mkdir()
@@ -37,17 +90,17 @@ def formatar_diretorio(origem, destino):
     shutil.rmtree(origem)
 
 
-def preparar_dataset1(dataset_dir):
+def preparar_dataset1(dataset_dir=Path("./dataset1")) -> None:
     """
-    Prepara o ambiente com o dataset "sarscov2-ctscan-dataset", baixado do kaggle
+    Prepara o ambiente com o dataset "sarscov2-ctscan-dataset", baixado do kaggle.
 
-    Se o dataset ainda não foi baixado, baixamos e descompactamos
+    Se o dataset ainda não foi baixado, baixamos e descompactamos.
 
-    :param dataset_dir: Um diretório pathlib.Path onde o dataset sera armazenado
-    :return:
+    :param dataset_dir: Um diretório onde o dataset será armazenado.
+    :return: None
     """
     if not dataset_dir.exists():
-        print("Baixando dataset de imagens e criando diretório...\n")
+        print("Baixando dataset 1 de imagens e criando diretório...\n")
         dataset_dir.mkdir()
 
         os.system(f'kaggle datasets download -d plameneduardo/sarscov2-ctscan-dataset -p {dataset_dir} --unzip -q')
@@ -60,22 +113,21 @@ def preparar_dataset1(dataset_dir):
 
         print("Pronto!")
     else:
-        print("Diretório de imagens já está presente na máquina. Prosseguindo...")
+        print("Diretório de imagens para o dataset 1 já está presente na máquina. Prosseguindo...")
 
 
-def preparar_dataset2(dataset_dir=Path("./dataset2")):
+def preparar_dataset2(dataset_dir=Path("./dataset2")) -> None:
     """
-    Prepara o ambiente com o dataset "preprocessed-ct-scans-for-covid19", baixado do kaggle
+    Prepara o ambiente com o dataset "preprocessed-ct-scans-for-covid19", baixado do kaggle.
 
-    Se o dataset ainda não foi baixado, baixamos e descompactamos
+    Se o dataset ainda não foi baixado, baixamos e descompactamos. Mantemos apenas as imagens originais,
+    e não as pré-processadas.
 
-    Mantemos apenas as imagens originais, e não as pré-processadas
-
-    :param dataset_dir: Um diretório pathlib.Path onde o dataset sera armazenado
-    :return:
+    :param dataset_dir: Um diretório onde o dataset será armazenado.
+    :return: None
     """
     if not dataset_dir.exists():
-        print("Baixando dataset de imagens e criando diretório...\n")
+        print("Baixando dataset 2 de imagens e criando diretório...\n")
         dataset_dir.mkdir()
 
         os.system(f'kaggle datasets download -d azaemon/preprocessed-ct-scans-for-covid19 -p {dataset_dir} --unzip -q')
@@ -92,16 +144,17 @@ def preparar_dataset2(dataset_dir=Path("./dataset2")):
         shutil.rmtree(dataset_dir / "Original CT Scans")
         print("Pronto!")
     else:
-        print("Diretório de imagens já está presente na máquina. Prosseguindo...")
+        print("Diretório de imagens pra o dataset 2 já está presente na máquina. Prosseguindo...")
 
 
-def mover_imagens(origem, num_imagens, destino):
-    """Move um número especificado de imagens de um diretório de origem para um destino.
+def mover_imagens(origem: Path, num_imagens: int, destino: str) -> None:
+    """
+    Move um número especificado de imagens de um diretório de origem para um destino.
 
-    :param origem: Nome do diretório de origem contendo as imagens.
+    :param origem: Diretório de origem contendo as imagens.
     :param num_imagens: O número de imagens para mover.
-    :param destino: Nome do diretório de destino (incluindo subdiretórios para classes).
-    :return:
+    :param destino: Nome do diretório de destino (incluindo subdiretório para a classe).
+    :return: None
     """
     destino_path = Path(destino)
     if not destino_path.exists():
@@ -118,11 +171,19 @@ def mover_imagens(origem, num_imagens, destino):
         shutil.move(src, dst)
 
 
-# padrao_regex = re.compile(r".*\.(jpg|jpeg|png)$", re.IGNORECASE)
-# if padrao_regex.match(image_name.name):
+def treinamento_e_teste(n: int, num_positivas: int, num_negativas: int, test_split: float, dataset_dir: Path) -> None:
+    """
+    Verifica se os diretórios de treinamento e teste já existem. Se não existirem, cria um diretório temporário,
+    copia as imagens do dataset original para esse diretório e então distribui as imagens entre os diretórios
+    de treinamento e teste conforme a proporção "test_split".
 
-
-def treinamento_e_teste(n, num_positivas, num_negativas, test_split, dataset_dir):
+    :param n: Número do dataset, utilizado para nomear os diretórios de treinamento e teste.
+    :param num_positivas: Número de exemplos positivos no dataset original.
+    :param num_negativas: Número de exemplos negativos no dataset original.
+    :param test_split: Proporção dos dados a serem utilizados para teste.
+    :param dataset_dir: Caminho do diretório contendo o dataset original.
+    :return: None
+    """
     if not (Path(f"treinamento{n}").exists() and Path(f"./teste{n}").exists()):
         print("\nCriando diretórios para treinamento e teste...")
 
@@ -147,12 +208,12 @@ def treinamento_e_teste(n, num_positivas, num_negativas, test_split, dataset_dir
         print("Diretórios de treinamento e teste já estão presentes. Prosseguindo...")
 
 
-def plot_images_from_dataset(ds, filename):
-    """Plota imagens de um tf.data.Dataset e salva a figura.
+def plotar_amostra(ds: tf.data.Dataset, filename: str) -> None:
+    """
+    Plota 9 imagens de um dataset e salva a figura.
 
-    :param ds: O número de imagens para plotar (padrão: 9).
+    :param ds: Dataset de onde tirar as imagens.
     :param filename: Nome do arquivo para salvar a figura.
-    :return:
     """
     imagens, labels = next(iter(ds.take(1)))
     label_indices = np.argmax(labels, axis=1)
@@ -166,3 +227,119 @@ def plot_images_from_dataset(ds, filename):
     plt.tight_layout()
     plt.savefig(filename, bbox_inches='tight')
     plt.close()
+
+
+def plotar_graficos(epocas: int, history: keras.callbacks.History, test_score: tuple[float, float],
+                    history_transfer: keras.callbacks.History, test_score_transfer: tuple[float, float],
+                    i: int, loss_max: float) -> None:
+    """
+    Plota os gráficos de acurácia e perda dos modelos treinados, com e sem transfer learning.
+    Inclui pontos de acurácia e perda de teste.
+    Garante que os eixos Y sejam os mesmos para todos os gráficos de acurácia e perda.
+
+    :param epocas: Número de épocas do treinamento
+    :param history: Histórico de treinamento do modelo sem transfer learning (objeto History do Keras).
+    :param history_transfer: Histórico de treinamento do modelo com transfer learning (objeto History do Keras).
+    :param test_score: Pontuação do teste do modelo sem transfer learning (vetor com perda na posição 0 e acurácia na posição 1).
+    :param test_score_transfer: Pontuação do teste do modelo com transfer learning (vetor com perda na posição 0 e acurácia na posição 1).
+    :param i: Número do dataset, usado para nomear os gráficos.
+    :param loss_max: Máximo das perdas entre os treinamentos
+
+    A função salva os seguintes gráficos:
+
+    - Acurácia do modelo sem transfer learning: './resultados/acuracia{i}.png'
+    - Perda do modelo sem transfer learning: './resultados/perda{i}.png'
+    - Acurácia do modelo com transfer learning: './resultados/acuracia_transfer{i}.png'
+    - Perda do modelo com transfer learning: './resultados/perda_transfer{i}.png'
+    """
+    loss_max += 0.1
+
+    # plotar loss normal
+    plt.title(f"Perda do Modelo {i}")
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.scatter(epocas - 1, test_score[0], color='red')
+    plt.annotate(f'{test_score[0]:.4f}', (epocas - 1, test_score[0]), textcoords="offset points", xytext=(0, 10),
+                 ha='center')
+    plt.xlabel("Época")
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))  # garante que só inteiros serão utilizados na escala
+    plt.ylabel("Perda")
+    plt.ylim(0, loss_max)
+    plt.legend(['train', 'validation', 'test'])
+    plt.savefig(f"./resultados/perda{i}.png", bbox_inches='tight')
+    plt.close()
+
+    # plotar acurácia normal
+    plt.title(f"Acurácia do Modelo {i}")
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.scatter(epocas - 1, test_score[1], color='red')
+    plt.annotate(f'{test_score[1]:.4f}', (epocas - 1, test_score[1]), textcoords="offset points", xytext=(0, 10),
+                 ha='center')
+    plt.xlabel("Época")
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))  # garante que só inteiros serão utilizados na escala
+    plt.ylabel("Acurácia")
+    plt.ylim(0, 1)
+    plt.legend(['train', 'validation', 'test'])
+    plt.savefig(f"./resultados/acuracia{i}.png", bbox_inches='tight')
+    plt.close()
+
+    # plotar loss com transfer learning
+    plt.title(f"Perda do Modelo {i} Com Transfer Learning")
+    plt.plot(history_transfer.history['loss'])
+    plt.plot(history_transfer.history['val_loss'])
+    plt.scatter(epocas - 1, test_score_transfer[0], color='red')
+    plt.annotate(f'{test_score_transfer[0]:.4f}', (epocas - 1, test_score_transfer[0]), textcoords="offset points",
+                 xytext=(0, 10), ha='center')
+    plt.xlabel("Época")
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))  # garante que só inteiros serão utilizados na escala
+    plt.ylabel("Perda")
+    plt.ylim(0, loss_max)
+    plt.legend(['train', 'validation', 'test'])
+    plt.savefig(f"./resultados/perda_transfer{i}.png", bbox_inches='tight')
+    plt.close()
+
+    # plotar acurácia com transfer learning
+    plt.title(f"Acurácia do Modelo {i} Com Transfer Learning")
+    plt.plot(history_transfer.history['accuracy'])
+    plt.plot(history_transfer.history['val_accuracy'])
+    plt.scatter(epocas - 1, test_score_transfer[1], color='red')
+    plt.annotate(f'{test_score_transfer[1]:.4f}', (epocas - 1, test_score_transfer[1]), textcoords="offset points",
+                 xytext=(0, 10), ha='center')
+    plt.xlabel("Época")
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))  # garante que só inteiros serão utilizados na escala
+    plt.ylabel("Acurácia")
+    plt.ylim(0, 1)
+    plt.legend(['train', 'validation', 'test'])
+    plt.savefig(f"./resultados/acuracia_transfer{i}.png", bbox_inches='tight')
+    plt.close()
+
+
+def salvar_resultados(N: tuple[list[int], list[int]], i: int, test_score: tuple[float, float],
+                      test_score_transfer: tuple[float, float], time: timedelta,
+                      time_transfer: timedelta) -> None:
+    """
+    Salva especificações do dataset e resultados do teste em um arquivo "resultados/info{i}.txt".
+
+    :param N: Uma tupla contendo duas listas de inteiros, onde a primeira lista representa o número de exemplos
+    positivos e a segunda lista o número de exemplos negativos para cada dataset.
+    :param i: Número do dataset, usado para nomear o arquivo de resultados.
+    :param test_score: Pontuação do teste do modelo sem transfer learning (uma tupla com perda na posição 0 e acurácia
+    na posição 1).
+    :param test_score_transfer: Pontuação do teste do modelo com transfer learning (uma tupla com perda na posição 0 e
+    acurácia na posição 1).
+    :param time: Tempo decorrido para o treinamento sem transfer learning (um objeto timedelta).
+    :param time_transfer: Tempo decorrido para o treinamento com transfer learning (um objeto timedelta).
+    :return: None
+    """
+    with open(f"resultados/info{i}.txt", "w") as file:
+        file.write(f"N: {N[i - 1][0] + N[i - 1][1]} imagens\n")
+        file.write(f"N positivas: {N[i - 1][0]} imagens\n")
+        file.write(f"N negativas: {N[i - 1][1]} imagens\n\n")
+        file.write(f"Tempo decorrido: {time}\n")
+        file.write(f"Perda do teste: {test_score[0]}\n")
+        file.write(f"Acurácia do teste: {test_score[1]}\n\n")
+        file.write(f"Tempo decorrido com transfer learning: {time_transfer}\n")
+        file.write(f"Perda do teste com transfer learning: {test_score_transfer[0]}\n")
+        file.write(f"Acurácia do teste com transfer learning: {test_score_transfer[1]}\n")
+        file.close()
--- vgg_19_covid_ct.py
+++ vgg_19_covid_ct.py
@@ -5,123 +5,140 @@ from random import seed, randint
 
 import tensorflow as tf
 from keras.callbacks import CSVLogger
-from matplotlib import pyplot as plt
-from matplotlib.ticker import MaxNLocator
 
-from functions import plot_images_from_dataset, preparar_dataset2, treinamento_e_teste, preparar_dataset1, \
-    prepararDiretorios
+from functions import salvar_parametros, prepararResultados, prepararDiretorios, plotar_amostra, plotar_graficos, \
+    salvar_resultados
 
-seed()
-
-# Parâmetros do treinamento --------------------------------------------------------------------------------------------
+# Parâmetros do treinamento ----------------------------------------------------------------------------------------
 base_learning_rate = 0.001
-epocas = 100
-
+epocas = 3
 test_split = 0.2
 validation_split = 0.4
-
 dataset_dir1 = Path("./dataset1")  # sarscov2-ctscan-dataset
 dataset_dir2 = Path("./dataset2")  # preprocessed-ct-scans-for-covid19
 
-N = prepararDiretorios(dataset_dir1, dataset_dir2, test_split)
 
-for i in [1, 2]:
-    # Criação de datasets keras ----------------------------------------------------------------------------------------
-    print("\nCriando datasets a partir dos diretórios...")
+def main():
+    seed()
 
-    train_ds, validation_ds = tf.keras.utils.image_dataset_from_directory(
-        directory=f"./treinamento{i}",
-        labels="inferred",
-        label_mode="categorical",
-        seed=randint(0, 100),
-        subset="both",
-        validation_split=validation_split,
-        batch_size=32,
-        image_size=(256, 256))
+    salvar_parametros(base_learning_rate, epocas, test_split, validation_split)
 
-    test_ds = tf.keras.utils.image_dataset_from_directory(
-        directory=f"./teste{i}",
-        labels="inferred",
-        label_mode="categorical",
-        batch_size=32,
-        image_size=(256, 256))
+    training, n = prepararResultados()
 
-    print("Pronto!\n")
+    histories, histories_transfer = [], []
+    test_scores, test_scores_transfer = [], []
 
-    # Aplicação da VGG-19 ----------------------------------------------------------------------------------------------
-    # criar a base do modelo
-    image_inputs2 = tf.keras.layers.Input(shape=(256, 256, 3))
-    VGG_19_base = tf.keras.applications.VGG19(include_top=False, weights="imagenet", input_tensor=image_inputs2,
-                                              pooling="max", classifier_activation="softmax")
+    loss_max = float('-inf')
 
-    # congelar a base para transfer learning
-    for _, layer in enumerate(VGG_19_base.layers):
-        layer.trainable = False
+    N = prepararDiretorios(dataset_dir1, dataset_dir2, test_split)
 
-    # layers para fine-tuning do transfer-learning
-    FC_layer_Flatten = tf.keras.layers.Flatten()(VGG_19_base.output)
-    x = tf.keras.layers.Dropout(0.3)(FC_layer_Flatten)
-    x = tf.keras.layers.Dense(units=128, activation="relu")(x)
-    x = tf.keras.layers.Dense(units=32, activation="relu")(x)
-    x = tf.keras.layers.Dense(units=8, activation="relu")(x)
-    outputs = tf.keras.layers.Dense(units=2, activation="softmax")(x)
+    for i in [1, 2]:
+        print(f"\nCriando datasets keras a partir do dataset {i}...")
 
-    # compilar o modelo final
-    modelo_final_transfer = tf.keras.Model(inputs=image_inputs2, outputs=outputs)
-    modelo_final_transfer.summary()
-    print("\n")
+        train_ds, validation_ds = tf.keras.utils.image_dataset_from_directory(
+            directory=f"./treinamento{i}",
+            labels="inferred",
+            label_mode="categorical",
+            seed=randint(0, 100),
+            subset="both",
+            validation_split=validation_split,
+            batch_size=32,
+            image_size=(256, 256))
 
-    modelo_final_transfer.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
-                                  loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
+        test_ds = tf.keras.utils.image_dataset_from_directory(
+            directory=f"./teste{i}",
+            labels="inferred",
+            label_mode="categorical",
+            batch_size=32,
+            image_size=(256, 256))
 
-    # criar um log em CSV para armazenar a história do modelo
-    csv_logger = CSVLogger(f"resultados/training{i}.csv", ",", False)
+        plotar_amostra(test_ds, f"resultados/sample{i}.png")
 
-    # treinamento
-    print(f"------------------------- Treinamento do modelo {i} -------------------------")
-    start_time = datetime.now()
-    history = modelo_final_transfer.fit(train_ds, validation_data=validation_ds, epochs=epocas, callbacks=[csv_logger],
-                                        verbose=2)
-    end_time = datetime.now()
+        print("Pronto!\n")
 
-    # teste
-    print(f"\n---------------------------- Teste do modelo {i} ----------------------------")
-    test_score = modelo_final_transfer.evaluate(test_ds)
+        # Aplicação da VGG-19 ------------------------------------------------------------------------------------------
+        # criar a base do modelo
+        image_inputs = tf.keras.layers.Input(shape=(256, 256, 3))
+        VGG_19_base = tf.keras.applications.VGG19(include_top=False, weights="imagenet", input_tensor=image_inputs,
+                                                  pooling="max", classifier_activation="softmax")
 
-    # Registro dos resultados ------------------------------------------------------------------------------------------
-    print("Salvando informações e resultados...\n\n")
+        # compilamos um modelo para treinamento completo
+        outputs = tf.keras.layers.Dense(units=2, activation="softmax")(VGG_19_base.output)
+        modelo_final = tf.keras.Model(inputs=image_inputs, outputs=outputs)
+        print("Modelo:")
+        modelo_final.summary()
+        print("\n")
 
-    # plotar uma imagem com exemplos de imagesn positivas e negativas
-    plot_images_from_dataset(test_ds, f"resultados/sample{i}.png")
+        modelo_final.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+                             loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
 
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title("Acurácia do Modelo")
-    plt.xlabel("Épocas")
-    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))  # garante que só inteiros serão utilizados na escala
-    plt.ylabel("Acurácia")
-    plt.legend(['train', 'validation'])
-    plt.savefig(f"./resultados/acuracia{i}.png", bbox_inches='tight')
-    plt.close()
+        # congelamos a base para transfer learning
+        for _, layer in enumerate(VGG_19_base.layers):
+            layer.trainable = False
 
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title("Perda do Modelo")
-    plt.xlabel("Época")
-    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))  # garante que só inteiros serão utilizados na escala
-    plt.ylabel("Perda")
-    plt.legend(['train', 'validation'])
-    plt.savefig(f"./resultados/perda{i}.png", bbox_inches='tight')
-    plt.close()
+        # adicionamos alguns layers para fine-tuning do transfer-learning
+        FC_layer_Flatten = tf.keras.layers.Flatten()(VGG_19_base.output)
+        x = tf.keras.layers.Dropout(0.3)(FC_layer_Flatten)
+        x = tf.keras.layers.Dense(units=128, activation="relu")(x)
+        x = tf.keras.layers.Dense(units=32, activation="relu")(x)
+        x = tf.keras.layers.Dense(units=8, activation="relu")(x)
+        outputs = tf.keras.layers.Dense(units=2, activation="softmax")(x)
 
-    with open(f"resultados/info{i}.txt", "w") as file:
-        file.write(f"N: {N[i]} imagens\n")
-        file.write(f"Test split: {test_split}\n")
-        file.write(f"Validation split: {validation_split}\n")
-        file.write(f"Learning rate: {base_learning_rate}\n")
-        file.write(f"Épocas: {epocas}\n")
-        file.write(f"Tempo decorrido: {end_time - start_time}\n")
-        file.write(f"Acurácia do deste: {test_score[1]}\n")
-        file.write(f"Perda do deste: {test_score[0]}")
+        # compilamos um modelo para treinamento com transfer learning
+        modelo_final_transfer = tf.keras.Model(inputs=image_inputs, outputs=outputs)
+        print("Modelo com transfer learning:")
+        modelo_final_transfer.summary()
+        print("\n")
 
-print("Pronto! Cheque a pasta resultados.")
+        modelo_final_transfer.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+                                      loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
+
+        # criamos logs em CSV para armazenar as histórias de cada modelo
+        csv_logger = CSVLogger(training / f"normal/dataset{i}_({n}).csv", ",", False)
+        csv_logger_transfer = CSVLogger(training / f"transfer/dataset{i}_({n}).csv", ",", False)
+
+        print(f"------------------------- Treinamento com dataset {i} -------------------------")
+        print("Normal:")
+        start_time = datetime.now()
+        history = modelo_final.fit(train_ds, validation_data=validation_ds, epochs=epocas,
+                                   callbacks=[csv_logger],
+                                   verbose=2)
+        end_time = datetime.now()
+        time = end_time - start_time
+
+        print("\nCom transfer learning:")
+        start_time = datetime.now()
+        history_transfer = modelo_final_transfer.fit(train_ds, validation_data=validation_ds, epochs=epocas,
+                                                     callbacks=[csv_logger_transfer],
+                                                     verbose=2)
+        end_time = datetime.now()
+        time_transfer = end_time - start_time
+
+        print(f"\n---------------------------- Teste do modelo {i} ----------------------------")
+        print("Normal:")
+        test_score = modelo_final.evaluate(test_ds)
+        print("\nCom transfer learning:")
+        test_score_transfer = modelo_final_transfer.evaluate(test_ds)
+
+        # Registro dos resultados --------------------------------------------------------------------------------------
+        print(f"\nSalvando resultados para o dataset {i}...\n\n")
+
+        histories.append(history)
+        histories_transfer.append(history_transfer)
+        test_scores.append(test_score)
+        test_scores_transfer.append(test_score_transfer)
+
+        loss_max = max(loss_max, max(history.history['loss']), max(history.history['val_loss']),
+                       max(history_transfer.history['loss']), max(history_transfer.history['val_loss']),
+                       test_score[0], test_score_transfer[0])
+
+        salvar_resultados(N, i, test_score, test_score_transfer, time, time_transfer)
+
+    print("Plotando gráficos dos resultados...")
+    for i in [1, 2]:
+        plotar_graficos(epocas, histories[i - 1], test_scores[i - 1], histories_transfer[i - 1],
+                        test_scores_transfer[i - 1], i, loss_max)
+    print("Pronto! Cheque a pasta resultados.")
+
+
+main()
